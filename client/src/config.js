41ec3e809ca84ddbf6e159eed17bcd5cea8a6fd1
Remy Schultz <remy@rschultz.dev>
2023-03-13 12:29:51 -0500

    fixed logout returnTo

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    SERVER_URL: 'http://localhost:5001'
+    SERVER_URL: 'http://localhost:5001',
+    CLIENT_ID: 'rJUlXGYCODlgjPKn5qslHgrmuuU9PLRs',
+    DOMAIN: 'dev-qhtirpu7ch1u66es.us.auth0.com'
 }
